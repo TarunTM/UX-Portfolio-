fd6ce47a2d991c697a27ea09f3959e107c91f954
TarunTM <tarunmadan2506@gmail.com>
2026-08-27 16:49:55 +0530

    fix(sanctum): remove unused ExternalLink import to fix Netlify build

--- src/pages/SanctumCaseStudy.tsx
+++ src/pages/SanctumCaseStudy.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ArrowLeft, ArrowRight, ExternalLink } from 'lucide-react';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 import Navbar from '../components/Navbar';
 import { projects } from '../data/projects';
 import heroVideo from '../assets/Sanctum Img:Video/Hero Video.webm';
